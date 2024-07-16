d9b502ed6c08f45d069d6221d6c0822dc1dea697
Sarvesh0906 <chaurasiasarvesh06@gmail.com>
2024-07-16 12:17:40 +0530

    newCommit

--- api/config.js
+++ api/config.js
@@ -5,9 +5,22 @@ dotenv.config();
 
 const dbURI = process.env.MONGODB_URI;
 
-const connectToMongoDB = () => { 
-    mongoose.connect(dbURI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+if (!dbURI) {
+  console.error('MONGODB_URI environment variable not set');
+  process.exit(1);
 }
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
+
 export { connectToMongoDB };
